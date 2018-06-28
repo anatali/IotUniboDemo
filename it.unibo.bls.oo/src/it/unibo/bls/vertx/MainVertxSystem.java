69969018896c4c8aea261b9d01a51780368b9b27
Angelo Croatti <a.croatti@unibo.it>
2018-06-28 17:41:19 +0200

    bls vertx based added

---
+++ it.unibo.bls.oo/src/it/unibo/bls/vertx/MainVertxSystem.java
@@ -0,0 +1,53 @@
+package it.unibo.bls.vertx;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+import io.vertx.core.Vertx;
+
+public class MainVertxSystem {
+
+	private JFrame frame;
+	
+	public MainVertxSystem() {
+		
+		Logger.activate();
+		
+		initFrame();
+		initTheSystem();
+	}
+
+	private void initFrame() {
+		frame = new JFrame();
+		frame.setSize(new Dimension(800, 300));
+		frame.setLayout(new BorderLayout());
+		frame.setTitle("BLS Vertx-based System");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	private void initTheSystem() {
+		final Vertx vertx = Vertx.vertx();
+		
+		final ButtonVertx button = new ButtonVertx(frame);
+		final LedVertx led = new LedVertx(frame);
+		
+		vertx.deployVerticle(button, res -> {
+			Logger.log(getClass(), res.succeeded() 
+					? "Button Verticle successfully deployed." 
+					: "Error during Button Verticle deployment.");
+		});
+		
+		vertx.deployVerticle(led, res -> {
+			Logger.log(getClass(), res.succeeded() 
+					? "Led Verticle successfully deployed." 
+					: "Error during Led Verticle deployment.");
+		});
+	}
+
+	public static void main(String[] args) {
+		new MainVertxSystem();
+	}
+}
